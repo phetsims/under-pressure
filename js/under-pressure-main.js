ce0a5e394a2860ca5a8135facb7c094de87a74cf
samreid <reids@colorado.edu>
2020-02-27 11:46:31 -0700

    ES6 module migration, see https://github.com/phetsims/chipper/issues/875

--- js/under-pressure-main.js
+++ js/under-pressure-main.js
@@ -3,30 +3,26 @@
 /**
  * Main file for the Under Pressure simulation.
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
-  const UnderPressureScreen = require( 'FLUID_PRESSURE_AND_FLOW/under-pressure/UnderPressureScreen' );
+import UnderPressureScreen from '../../fluid-pressure-and-flow/js/under-pressure/UnderPressureScreen.js';
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import underPressureStrings from './under-pressure-strings.js';
 
-  // strings
-  const underPressureTitleString = require( 'string!UNDER_PRESSURE/under-pressure.title' );
+const underPressureTitleString = underPressureStrings[ 'under-pressure' ].title;
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Sam Reid',
-      softwareDevelopment: 'John Blanco, Aadish Gupta, Sam Reid',
-      team: 'Bryce Gruneich, Trish Loeblein, Ariel Paul, Kathy Perkins, Rachel Pepper, Noah Podolefsky',
-      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Elise Morgan, Ben Roberts',
-      thanks: 'Thanks to Mobile Learner Labs and Actual Concepts for working with the PhET development team to ' +
-              'convert this simulation to HTML5.'
-    }
-  };
+const simOptions = {
+  credits: {
+    leadDesign: 'Sam Reid',
+    softwareDevelopment: 'John Blanco, Aadish Gupta, Sam Reid',
+    team: 'Bryce Gruneich, Trish Loeblein, Ariel Paul, Kathy Perkins, Rachel Pepper, Noah Podolefsky',
+    qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Elise Morgan, Ben Roberts',
+    thanks: 'Thanks to Mobile Learner Labs and Actual Concepts for working with the PhET development team to ' +
+            'convert this simulation to HTML5.'
+  }
+};
 
-  SimLauncher.launch( function() {
-    const sim = new Sim( underPressureTitleString, [ new UnderPressureScreen() ], simOptions );
-    sim.start();
-  } );
+SimLauncher.launch( function() {
+  const sim = new Sim( underPressureTitleString, [ new UnderPressureScreen() ], simOptions );
+  sim.start();
 } );
---
+++ js/under-pressure-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import underPressure from './underPressure.js';
+
+const underPressureStrings = getStringModule( 'UNDER_PRESSURE' );
+
+underPressure.register( 'underPressureStrings', underPressureStrings );
+
+export default underPressureStrings;
